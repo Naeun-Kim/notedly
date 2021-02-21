8239edf90edbae30a9b4c84295bc82ac4fb526f7
naeun kim <naeunkim@naeunui-MacBookAir.local>
2021-02-21 15:11:13 +0900

    create notedly app client UI

---
+++ src/pages/home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+import Header from '../components/Header';
+import Navigation from '../components/Navigation';
+
+const Home = () => {
+    return (
+        <div>
+            <Header />
+            <Navigation />
+        </div>
+    )
+}
+
+export default Home;
